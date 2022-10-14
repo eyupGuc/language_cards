74425e9dea4282ea4955bd7faeb389b46c9e978b
eyupGuc <meydanguresi@gmail.com>
2022-10-14 23:23:12 +0300

    v

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Card from './components/card/Card';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello React</h1>
+    <Card/>
     </div>
   );
 }
---
+++ src/components/card/Card.jsx
@@ -0,0 +1,8 @@
+import {languages} from '../helper/data';
+
+const Card=()=>{
+    console.log(languages);
+    return (<h1>Hello Js</h1>)
+}
+
+export default Card;
